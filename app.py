a488a3f7aa746993e4dfab8694b7e37b49c169c8
Sudhamsu Bairisetti <Sudhamsu Bairisetti@DESKTOP-0J3P9UI>
2023-01-29 21:13:42 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request,session,logging,url_for,redirect,flash,g
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route("/",methods=['GET','POST'])
+def login():
+    userName = ""
+    password = ""
+    error = None
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    return render_template('login.html',error=error)
+
+@app.route("/signup",methods=['POST','GET'])
+    firstName = ""
+    lastName = ""
+    email = ""
+    userName = ""
+    password = ""
+def signup():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    return render_template("signup.html")
+
+@app.route("/home",methods=['POST','GET'])
+def home():
+    return render_template("home.html")
+
+
+if __name__== "__main__":
+    app.run(debug=True)
